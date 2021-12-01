37f615f25ceea63dca0eb1a7a79d103cb06eebd3
Ladányi Bence <ladanyi.bence96@gmail.com>
2021-12-01 09:41:13 +0100

    first task put into function

--- 01_SonarSweep/SonarSweep.1.ts
+++ 01_SonarSweep/SonarSweep.1.ts
@@ -7,16 +7,20 @@ const data: String = fs.readFileSync('SonarSweepINput.1.txt', {
 
 const dataArr: number[] = data.split('\n').map((e) => parseInt(e));
 
-const inc_dec: number[] = dataArr.map((e: number, i: number) => {
-  if (i === 0) {
-    return 0;
-  } else if (e > dataArr[i - 1]) {
-    return 1;
-  } else {
-    return 0;
-  }
-});
+function increasingSum(data: number[]): number {
+  const inc_dec: number[] = data.map((e: number, i: number) => {
+    if (i === 0) {
+      return 0;
+    } else if (e > data[i - 1]) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
 
-const sum = inc_dec.reduce((acc: number, curr: number) => acc + curr);
+  const sum = inc_dec.reduce((acc: number, curr: number) => acc + curr);
 
-console.log(sum);
+  return sum;
+}
+
+console.log(increasingSum(dataArr));
