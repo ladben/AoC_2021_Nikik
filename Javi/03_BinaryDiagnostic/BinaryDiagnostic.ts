1998eaec5582abb4792a84779ed9d9d9634724b6
Ladányi Bence <ladanyi.bence96@gmail.com>
2021-12-03 11:33:27 +0100

    Day03 completed. Code is ugly af

--- Javi/03_BinaryDiagnostic/BinaryDiagnostic.ts
+++ Javi/03_BinaryDiagnostic/BinaryDiagnostic.ts
@@ -9,13 +9,50 @@ const data: string = fs.readFileSync(
   }
 );
 
-const dataStringArr: string[] = data.split('\n');
+function generateNumArr(length: number, value: number): number[] {
+  let arr: number[] = [];
+  for (let i: number = 0; i < length; i++) {
+    arr.push(value);
+  }
 
-let numberOfOnes: number[] = [];
-const numberOfData: number = dataStringArr.length;
+  return arr;
+}
 
-for (let i: number = 0; i < dataStringArr[0].length; i++) {
-  numberOfOnes.push(0);
+function countOnes(data: string[], lengthOfValues: number): number[] {
+  let numberOfOnes: number[] = generateNumArr(lengthOfValues, 0);
+
+  data.forEach((e: string) => {
+    e.split('')
+      .map((e: string) => parseInt(e))
+      .forEach((e: number, i: number) => {
+        numberOfOnes[i] += e;
+      });
+  });
+
+  return numberOfOnes;
+}
+
+function getRate(
+  numberOfOnes: number[],
+  numberOfData: number,
+  mostLeast: number
+): number[] {
+  const rate: number[] = numberOfOnes.map((e: number) => {
+    const numberOfDigitOne: number = e;
+    const numberOfDigitZero: number = numberOfData - e;
+
+    if (numberOfDigitOne >= numberOfDigitZero) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  if (!mostLeast) {
+    return invertRate(rate);
+  } else {
+    return rate;
+  }
 }
 
 function invertRate(rate: number[]): number[] {
@@ -33,26 +70,77 @@ function getDecimalOfRate(rate: number[]): number {
   return parseInt(rateStr, 2);
 }
 
-dataStringArr.forEach((e: string) => {
-  e.split('')
-    .map((e: string) => parseInt(e))
-    .forEach((e: number, i: number) => {
-      numberOfOnes[i] += e;
-    });
-});
+const dataStringArr: string[] = data.split('\n');
+const numberOfData: number = dataStringArr.length;
+let numberOfOnes: number[] = countOnes(dataStringArr, dataStringArr[0].length);
 
-const gammaRate: number[] = numberOfOnes.map((e: number) => {
-  const numberOfDigitOne: number = e;
-  const numberOfDigitZero: number = numberOfData - e;
+const gammaRate: number[] = getRate(numberOfOnes, numberOfData, 1);
 
-  if (numberOfDigitOne > numberOfDigitZero) {
-    return 1;
-  } else {
-    return 0;
-  }
-});
-
-const epsilonRate: number[] = invertRate(gammaRate);
+const epsilonRate: number[] = getRate(numberOfOnes, numberOfData, 0);
 
 console.log('Power Consumption:');
 console.log(getDecimalOfRate(gammaRate) * getDecimalOfRate(epsilonRate));
+
+// ------------------ part two --------------------------
+
+function filterData(
+  data: string[],
+  nthDigitToWatch: number,
+  mostLeast: number
+): string[] {
+  const numberOfOnes: number[] = countOnes(data, data[0].length);
+  console.log(
+    `Number of ones when watching ${nthDigitToWatch}nth digit: ${numberOfOnes}`
+  );
+  console.log(`Length of data: ${data.length}`);
+  const numberOfOnesInNthPlace: number = numberOfOnes[nthDigitToWatch];
+  const numberOfZerosInNthPlace: number = data.length - numberOfOnesInNthPlace;
+  let digitToKeep: number;
+
+  if (!mostLeast) {
+    if (numberOfOnesInNthPlace < numberOfZerosInNthPlace) {
+      digitToKeep = 1;
+    } else {
+      digitToKeep = 0;
+    }
+  } else {
+    if (numberOfOnesInNthPlace >= numberOfZerosInNthPlace) {
+      digitToKeep = 1;
+    } else {
+      digitToKeep = 0;
+    }
+  }
+
+  console.log(`digit to keep: ${digitToKeep}`);
+  console.log(`Data to filter: ${data}`);
+
+  return data.filter((e: string) => {
+    const digitInNthPlaceInElement: number = parseInt(e[nthDigitToWatch]);
+    return digitInNthPlaceInElement === digitToKeep;
+  });
+}
+
+function reduceData(
+  data: string[],
+  mostLeast: number,
+  nthDigitToWatch: number
+): string {
+  if (data.length === 1) {
+    return data[0];
+  } else {
+    const newData: string[] = filterData(data, nthDigitToWatch, mostLeast);
+    return reduceData(newData, mostLeast, nthDigitToWatch + 1);
+  }
+}
+
+const oxygenGeneratorRating: number[] = reduceData(dataStringArr, 1, 0)
+  .split('')
+  .map((e: string) => parseInt(e));
+const CO2ScrubberRating: number[] = reduceData(dataStringArr, 0, 0)
+  .split('')
+  .map((e: string) => parseInt(e));
+
+console.log('Life Support Rating:');
+console.log(
+  getDecimalOfRate(oxygenGeneratorRating) * getDecimalOfRate(CO2ScrubberRating)
+);
