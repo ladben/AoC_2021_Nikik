7ffdde3c42d5614cf899f045e7434ecc1d28937e
Ladányi Bence <ladanyi.bence96@gmail.com>
2021-12-06 17:24:05 +0100

    maybe the simplest solution still cannot handle 256 days

---
+++ Javi/06_Lanternfish/Lanternfish_maybeTheSimplest.ts
@@ -0,0 +1,25 @@
+import * as path from 'path';
+import * as fs from 'fs';
+
+const data: string = fs.readFileSync(
+  path.resolve(__dirname, '../Inputs/06_Lanternfish.txt'),
+  { encoding: 'utf8', flag: 'r' }
+);
+
+let dataNumArr: number[] = data.split(',').map((e: string) => parseInt(e));
+
+for (let i: number = 0; i < 256; i++) {
+  let newlySpawnFishArr: number[] = [];
+  dataNumArr = dataNumArr.map((e: number) => {
+    if (e === 0) {
+      newlySpawnFishArr.push(8);
+      return 6;
+    } else {
+      return e - 1;
+    }
+  });
+
+  dataNumArr = dataNumArr.concat(newlySpawnFishArr);
+}
+
+console.log(dataNumArr.length);
